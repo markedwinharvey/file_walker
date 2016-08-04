1de9acb35fe3a3c43b10a6c77a88e97be9fade43
Mark Harvey <markedwinharvey@gmail.com>
2016-08-04 03:05:04 -0700

    reorganize, remove redundancy, add size

--- filewalker/filewalker.py
+++ filewalker/filewalker.py
@@ -22,7 +22,7 @@ files,dirs,ftree = fw.walk(root=root, max_depth=max_depth)
 #Usage:
 #
 for f in files:
-	print f.name, f.rel, f.abs		#rel=relative, abs=absolute paths; dirs node list is structurally identical
+	print f.name, f.rel, f.abs, f.size		#rel=relative, abs=absolute paths; dirs node list is structurally identical
 
 
 Optional arguments:
@@ -48,62 +48,46 @@ import subprocess as sp
 import os
 import sys
 
-def walk(**kwargs):
-	'''  accept keyword arguments 'root', 'max_depth' and 'print_all' '''
-	print_all = True
-	if 'print_all' in kwargs.keys():
-		print_all = kwargs['print_all']
+#------class declarations------#
+
+class file_tree():
+	def __init__(self,root):
+		self.root=root
+
+class file():
+	def __init__(self,name,rel,abs,size):
+		self.name=name
+		self.rel=rel
+		self.abs=abs
+		self.size=size
+
+class dir():
+	def __init__(self,name,rel,abs):
+		self.name=name
+		self.rel=rel
+		self.abs=abs
+		self.children=[]
+
+
+#------general functions------#
+
+def exit():
 	if print_all:
-		print '#--------------------------------#'
-		print '#-------- filewalker.py ---------#'
-		print '#--------------------------------#'
-	
-	def exit():
-		if print_all:
-			print; print 'Exiting...'; print
-		sys.exit()
-	
-	class file_tree():
-		def __init__(self,root):
-			self.root=root
-	
-	class node():
-		def __init__(self, path, parent, is_dir, is_root):
-			self.path=path
-			self.parent=parent
-			self.is_dir=is_dir
-			self.is_root=is_root
-			self.children=[]
-	
-	class file():
-		def __init__(self,name,rel,abs):
-			self.name=name
-			self.rel=rel
-			self.abs=abs
-	
-	class dir():
-		def __init__(self,name,rel,abs):
-			self.name=name
-			self.rel=rel
-			self.abs=abs
-			
-	#initialize some things
-	curr_depth = 0
-	all_file_list = []
-	all_dir_list = []
-	curr_node_list = []
-	
-	#initialize root and depth
+		print; print 'Exiting...'; print
+	sys.exit()
+
+def get_root(kwargs):
 	root = sp.Popen(['pwd'],stdout=sp.PIPE).communicate()[0].replace('\n','')
-	max_depth = None
-	
 	if 'root' in kwargs.keys():
 		root = kwargs['root']
 		if not os.path.exists(root):
 			if print_all:
 				print 'not a viable path'
 			exit()
-			
+	return root
+
+def get_max_depth(kwargs):
+	max_depth = None
 	if 'max_depth' in kwargs.keys() and kwargs['max_depth'] is not None:
 		max_depth = kwargs['max_depth']
 		try:
@@ -114,14 +98,48 @@ def walk(**kwargs):
 			exit()
 		if max_depth < 0:
 			max_depth = 0
-	
-	new_node = node(path=root, parent=None, is_dir=True, is_root=True)
-	f_tree = file_tree(new_node)
+	return max_depth
 
-	curr_node_list.append(new_node)
+
+#------walk functions------#
+
+def walk(**kwargs):
+	'''  accept keyword arguments 'root', 'max_depth' and 'print_all' '''
+	
+	print_all = True
+	if 'print_all' in kwargs.keys():
+		print_all = kwargs['print_all']
+		
+	if print_all:
+		print '#--------------------------------#'
+		print '#-------- filewalker.py ---------#'
+		print '#--------------------------------#'
+	
+	#---initialize some things---#
+	curr_depth = 0
+	all_file_list = []
+	all_dir_list = []
+	curr_node_list = []
+	
+	root = get_root(kwargs)
+	max_depth = get_max_depth(kwargs)
+	
+	new_node = dir(
+		name	= root.split('/')[-1],
+		rel		= '',
+		abs		= root
+	)
+	
+	f_tree = file_tree(new_node)
+	curr_node_list.append(new_node)	
+	
+	if print_all:
+		print 'Scanning:',root
+		print root.split('/')[-1]
 	
 	def do_walk(curr_depth, curr_node_list):
-		this_dir_path = curr_node_list[-1].path
+		
+		this_dir_path = curr_node_list[-1].abs
 		
 		cmd = 'ls \'' + str(this_dir_path)+'\''
 		folder_contents = [x for x in sp.Popen([cmd],stdout=sp.PIPE,shell=True).communicate()[0].split('\n') if x]
@@ -129,58 +147,57 @@ def walk(**kwargs):
 		if not folder_contents:
 			curr_node_list.pop()
 			return curr_depth - 1, curr_node_list
-			
+		
+		file_size_sum = 0
 		
 		for this_node in folder_contents:
-			if print_all:
-				print curr_depth*'---'+this_node
+			
 			this_node_path = os.path.abspath(os.path.join(this_dir_path,this_node))
 			
-			#convert to rel path
-			this_node_rel_path = this_node_path[len(root)+1:]
+			this_node_rel_path = this_node_path[len(root)+1:]	#convert to rel path
 				
 			if not os.path.isdir(this_node_path):		
-				#node is file
-				all_file_list.append( 
-					file(
-						name=this_node,
-						rel=this_node_rel_path,
-						abs=this_node_path
-					)
+				'''node is file'''
+				
+				new_node = file(
+					name	= this_node,
+					rel		= this_node_rel_path,
+					abs		= this_node_path,
+					size	= os.path.getsize(this_node_path)
 				)
-				new_node = node(
-					path=this_node_path, 
-					parent=curr_node_list[-1], 
-					is_dir=False, 
-					is_root=False
-				)					
-				curr_node_list[-1].children.append(new_node)
-
-			else:									
-				#node is dir
-				all_dir_list.append(
-					dir(
-						name=this_node,
-						rel=this_node_rel_path,
-						abs=this_node_path
-					)
-				)
-				new_node = node(
-					path=this_node_path, 
-					parent=curr_node_list[-1], 
-					is_dir=True, 
-					is_root=False
-				)
+				all_file_list.append(new_node)
+		
 				curr_node_list[-1].children.append(new_node)
 				
+				if print_all: 
+					print ' |'+curr_depth*'-'+this_node+' -> ('+str(all_file_list[-1].size)+' bytes)'
+			
+			else:									
+				'''node is dir'''
+							
+				new_node = dir(
+					name	= this_node,
+					rel		= this_node_rel_path,
+					abs		= this_node_path
+				)
+				all_dir_list.append(new_node)
+				
+				curr_node_list[-1].children.append(new_node)
+				if print_all: 
+					print ' |'+curr_depth*'---'+this_node	
+				
 				if max_depth is None or curr_depth < max_depth:
-					#recurse through directories
+					'''recurse through directories'''
 					curr_depth+=1
 					curr_node_list.append(new_node)
 					curr_depth, curr_node_list = do_walk(curr_depth, curr_node_list)					
 				else:
+				
 					return curr_depth -1, curr_node_list
-
+		else:	#contents of folder explored
+			pass	
+			
+					
 		return curr_depth - 1, curr_node_list
 	
 	curr_depth, curr_node_list = do_walk(curr_depth, curr_node_list)
